7b4a66682b8674aaa81bdae5fe856f86833267eb
Simon Vergauwen <nomisRev@users.noreply.github.com>
2022-07-29 12:37:16 +0200

    Enable plugins, and format project

--- src/commonMain/kotlin/arrow/continuations/unsafe/unsafe.kt
+++ src/commonMain/kotlin/arrow/continuations/unsafe/unsafe.kt
@@ -1,11 +1,11 @@
 package arrow.continuations.unsafe
 
-import kotlinx.coroutines.CoroutineScope
 import kotlin.coroutines.CoroutineContext
+import kotlinx.coroutines.CoroutineScope
 
 expect object Unsafe {
   fun onShutdown(block: suspend () -> Unit): () -> Unit
-  
+
   fun runCoroutineScope(
     context: CoroutineContext,
     block: suspend CoroutineScope.() -> Unit,
