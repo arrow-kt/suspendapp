bc9d90b30a5c9b46b3e673e8550c3aad7cfeedbb
Simon Vergauwen <nomisRev@users.noreply.github.com>
2022-07-29 13:18:35 +0200

    Set groupId, and setup Dokka

--- src/commonMain/kotlin/arrow/continuations/SuspendApp.kt
+++ src/commonMain/kotlin/arrow/continuations/SuspendApp.kt
@@ -10,57 +10,22 @@ import kotlinx.coroutines.Job
 import kotlinx.coroutines.cancelAndJoin
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withTimeout
+import kotlin.coroutines.EmptyCoroutineContext
 
 /**
- * # Module SuspendApp
+ * An unsafe blocking edge that wires the [CoroutineScope] (and structured concurrency) to the [SuspendApp],
+ * such that the [CoroutineScope] gets cancelled when the `App` is requested to gracefully shutdown.
+ *   => `SIGTERM` & `SIGINT` on Native & NodeJS and a ShutdownHook for JVM.
  *
- * # Rationale When building applications that require graceful shutdown it typically requires us to
- * write a bunch of platform specific code. This library offers a common API to write such
- * applications targeting the JVM, Native or NodeJS.
+ * It applies backpressure to the process such that they can gracefully shutdown.
  *
- * ## SuspendApp
+ * @param context the [CoroutineContext] where [block] will execute.
+ * Use [EmptyCoroutineContext] to create an `CoroutineDispatcher` for the main thread and run there instead.
  *
- * SuspendApp allows for building applications that leverage `CoroutineScope`, and structured
- * concurrency as a way of reasoning about resource-safety. When we're writing apps that need to be
- * able to _gracefully shutdown_, then you want your resource handlers to run on SIGTERM, SIGINT or
- * JVM Shutdown.
+ * @param timeout the maximum backpressure time that can be applied to the process.
+ * This emulates a `SIGKILL` command, and after the [timeout] is passed the App will forcefully shut down regardless of finalizers.
  *
- * Let's look at some examples.
- *
- * ```kotlin
- * import arrow.continuations.SuspendApp
- * import kotlinx.coroutines.CancellationException
- * import kotlinx.coroutines.NonCancellable
- * import kotlinx.coroutines.delay
- * import kotlinx.coroutines.withContext
- *
- * fun main() = SuspendApp {
- *   try {
- *     println("App Started!  Waiting until asked to shutdown.")
- *     while (true) {
- *       delay(2_500)
- *       println("Ping")
- *     }
- *   } catch (e: CancellationException) {
- *     println("Cleaning up App... will take 10 seconds...")
- *     withContext(NonCancellable) { delay(10_000) }
- *     println("Done cleaning up. Will release app to exit")
- *   }
- * }
- * ```
- * ```text
- * App Started! Waiting until asked to shutdown.
- * Ping
- * Closing resources..................... Done!
- *
- * Process finished with exit code 130 (interrupted by signal 2: SIGINT)
- * ```
- *
- * # SuspendApp with Arrow Fx Resource TODO
- *
- * ## SuspendApp with Arrow Fx Resource, Ktor & K8s TODO
- *
- * ## SuspendApp with Kotlin-Kafka TODO
+ * @param block the lambda of the actual application.
  */
 fun SuspendApp(
   context: CoroutineContext = Dispatchers.Default,
