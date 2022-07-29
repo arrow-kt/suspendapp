de3a2e2c88a0b4ebb0c39e5dd5619ba7d2aea37c
Simon Vergauwen <nomisRev@users.noreply.github.com>
2022-07-29 13:38:20 +0200

    SpotlessApply

--- src/commonMain/kotlin/arrow/continuations/SuspendApp.kt
+++ src/commonMain/kotlin/arrow/continuations/SuspendApp.kt
@@ -2,6 +2,7 @@ package arrow.continuations
 
 import arrow.continuations.unsafe.Unsafe
 import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.EmptyCoroutineContext
 import kotlin.time.Duration
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.CoroutineStart
@@ -10,20 +11,20 @@ import kotlinx.coroutines.Job
 import kotlinx.coroutines.cancelAndJoin
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withTimeout
-import kotlin.coroutines.EmptyCoroutineContext
 
 /**
- * An unsafe blocking edge that wires the [CoroutineScope] (and structured concurrency) to the [SuspendApp],
- * such that the [CoroutineScope] gets cancelled when the `App` is requested to gracefully shutdown.
- *   => `SIGTERM` & `SIGINT` on Native & NodeJS and a ShutdownHook for JVM.
+ * An unsafe blocking edge that wires the [CoroutineScope] (and structured concurrency) to the
+ * [SuspendApp], such that the [CoroutineScope] gets cancelled when the `App` is requested to
+ * gracefully shutdown. => `SIGTERM` & `SIGINT` on Native & NodeJS and a ShutdownHook for JVM.
  *
  * It applies backpressure to the process such that they can gracefully shutdown.
  *
- * @param context the [CoroutineContext] where [block] will execute.
- * Use [EmptyCoroutineContext] to create an `CoroutineDispatcher` for the main thread and run there instead.
+ * @param context the [CoroutineContext] where [block] will execute. Use [EmptyCoroutineContext] to
+ * create an `CoroutineDispatcher` for the main thread and run there instead.
  *
- * @param timeout the maximum backpressure time that can be applied to the process.
- * This emulates a `SIGKILL` command, and after the [timeout] is passed the App will forcefully shut down regardless of finalizers.
+ * @param timeout the maximum backpressure time that can be applied to the process. This emulates a
+ * `SIGKILL` command, and after the [timeout] is passed the App will forcefully shut down regardless
+ * of finalizers.
  *
  * @param block the lambda of the actual application.
  */
