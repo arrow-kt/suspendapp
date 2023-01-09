c0a59da316a9eee2ce95707a35cdd4290ea4932a
Jose Gutiérrez de Ory <jose.gutierrez@47deg.com>
2023-01-09 09:15:59 +0100

    Update suspendapp-ktor/build.gradle.kts
    Co-authored-by: Simon Vergauwen <nomisRev@users.noreply.github.com>

--- suspendapp-ktor/build.gradle.kts
+++ suspendapp-ktor/build.gradle.kts
@@ -11,9 +11,45 @@ repositories {
     mavenCentral()
 }
 
-dependencies{
-  implementation(libs.coroutines)
-  implementation(libs.arrow.fx)
-  implementation(libs.ktor.core)
-  implementation(libs.ktor.netty)
+kotlin {
+  // We set up custom targets rather than use Arrow Gradle Config for Kotlin,
+  // Since we don't support all targets but only subset where having this behavior makes sense.
+  jvm {
+    compilations.all {
+      kotlinOptions.jvmTarget = "1.8"
+    }
+  }
+  js(IR) {
+    nodejs()
+  }
+  
+  linuxX64()
+  mingwX64()
+  macosArm64()
+  macosX64()
+  
+  sourceSets {
+    val commonMain by getting {
+      dependencies {
+        implementation(libs.coroutines)
+        implementation(libs.arrow.fx)
+        implementation(libs.ktor.core)
+      }
+    }
+    
+    val jvmMain by getting
+    val jsMain by getting
+    val mingwX64Main by getting
+    val linuxX64Main by getting
+    val macosArm64Main by getting
+    val macosX64Main by getting
+    
+    create("nativeMain") {
+      dependsOn(commonMain)
+      mingwX64Main.dependsOn(this)
+      linuxX64Main.dependsOn(this)
+      macosArm64Main.dependsOn(this)
+      macosX64Main.dependsOn(this)
+    }
+  }
 }
