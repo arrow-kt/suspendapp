27c6e1abdb373c7768bee9f8c5db332d2b770c9e
Simon Vergauwen <nomisRev@users.noreply.github.com>
2023-08-29 08:52:30 +0200

    Fix changes Kotlin 1.9.x

--- suspendapp-ktor/src/nativeMain/kotlin/arrow/continuations/ktor/WORKING_DIRECTORY_PATH.kt
+++ suspendapp-ktor/src/nativeMain/kotlin/arrow/continuations/ktor/WORKING_DIRECTORY_PATH.kt
@@ -3,9 +3,10 @@ package arrow.continuations.ktor
 import kotlinx.cinterop.*
 import platform.posix.*
 
+@OptIn(ExperimentalForeignApi::class)
 internal actual val WORKING_DIRECTORY_PATH: String
   get() = memScoped {
     val result = allocArray<ByteVar>(512)
-    getcwd(result, 512)
+    getcwd(result, 512u)
     result.toKString()
   }
