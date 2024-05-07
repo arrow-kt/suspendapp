1df89b4cd042cbdbbca9456cf70d91c66cee462a
Marc Moreno Ferrer <ignis.fatue@gmail.com>
2024-05-07 10:02:50 +0200

    Add server constructor with ApplicationEngineEnvironment

--- suspendapp-ktor/src/commonMain/kotlin/arrow/continuations/ktor/KtorServer.kt
+++ suspendapp-ktor/src/commonMain/kotlin/arrow/continuations/ktor/KtorServer.kt
@@ -63,6 +63,41 @@ suspend fun <
     engine.environment.log.info("HTTP server shutdown!")
   }
 
+/**
+ * Ktor [ApplicationEngine] as a [Resource]. This [Resource] will gracefully shut down the server
+ * When we need to shut down a Ktor service we need to properly take into account a _grace_ period
+ * where we still handle requests instead of immediately cancelling any in-flight requests.
+ *
+ * @param factory Application engine for processing the requests
+ * @param environment definition of the environment where the engine will run
+ * @param preWait preWait a duration to wait before beginning the stop process. During this time,
+ *   requests will continue to be accepted. This setting is useful to allow time for the container
+ *   to be removed from the load balancer. This is disabled when `io.ktor.development=true`.
+ * @param grace grace a duration during which already inflight requests are allowed to continue
+ *   before the shutdown process begins.
+ * @param timeout timeout a duration after which the server will be forceably shutdown.
+ */
+suspend fun <
+  TEngine : ApplicationEngine, TConfiguration : ApplicationEngine.Configuration> ResourceScope
+  .server(
+  factory: ApplicationEngineFactory<TEngine, TConfiguration>,
+  environment: ApplicationEngineEnvironment,
+  preWait: Duration = 30.seconds,
+  grace: Duration = 500.milliseconds,
+  timeout: Duration = 500.milliseconds
+): ApplicationEngine =
+  install({ embeddedServer(factory, environment).apply(ApplicationEngine::start) }) { engine, _ ->
+    if (!engine.environment.developmentMode) {
+      engine.environment.log.info(
+        "prewait delay of ${preWait.inWholeMilliseconds}ms, turn it off using io.ktor.development=true"
+      )
+      delay(preWait.inWholeMilliseconds)
+    }
+    engine.environment.log.info("Shutting down HTTP server...")
+    engine.stop(grace.inWholeMilliseconds, timeout.inWholeMicroseconds)
+    engine.environment.log.info("HTTP server shutdown!")
+  }
+
 // Ported from Ktor:
 // https://github.com/ktorio/ktor/blob/0de7948fbe3f78673f4f90de9c5ea5986691819a/ktor-server/ktor-server-host-common/jvmAndNix/src/io/ktor/server/engine/ServerEngineUtils.kt
 internal expect val WORKING_DIRECTORY_PATH: String
